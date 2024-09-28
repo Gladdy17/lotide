124d5f4aa500efae7889bf147d5eadfb4410d9cd
Gladdy17 <gladston.bosco@gmail.com>
2024-09-28 12:55:59 -0600

    Pushed changes with the feedback

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,12 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅ ✅ ✅ Assertion Passed: [actual] === [expected]");
-  } else {
-    console.log("🛑 🛑 🛑 Assertion Failed: [actual] !== [expected]");
+  if(actual === expected){
+    console.log( "✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}")
+  }else {
+    console.log("🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}")
   }
 };
 
 assertEqual("Test", "Test");
+assertEqual("1", "1");
+assertEqual("Testing", "Test");
+assertEqual(2, 2.5);
