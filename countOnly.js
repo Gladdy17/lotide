99f16545adb3daa4a13835e93c1ad3b7cb4a6d8b
Gladdy17 <gladston.bosco@gmail.com>
2024-10-04 17:06:02 -0600

    Added changes after feedback from AI

--- countOnly.js
+++ countOnly.js
@@ -11,11 +11,13 @@ const countOnly = function (allItems, itemsToCount) {
   const results= {};
   for (const item of allItems){
     console.log(item);
-  if (results[item]) {
+  if (itemsToCount[item]) {
+   if (results[item]) {
    results[item] += 1;
  } else {
    results[item] = 1;
  }
+    }
   }
   return results;
 };
