07b92f5278f365b47ae493aa3c61851b9be2b70c
Gladdy17 <gladston.bosco@gmail.com>
2024-10-06 10:23:21 -0600

    Added changes as requested

---
+++ eqObjects.js
@@ -0,0 +1,48 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const obj1 = Object.keys(object1);
+  const obj2 = Object.keys(object2);
+  if (obj1.length !== obj2.length) {
+    return false;
+  }
+  for (const key of obj1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
