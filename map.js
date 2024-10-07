b2ef0dc25ffaa30b07a4d1a735020a086d26e5b5
Gladdy17 <gladston.bosco@gmail.com>
2024-10-07 13:37:28 -0600

    Map.js

--- map.js
+++ map.js
@@ -18,4 +18,24 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2]);
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function (array, callback) {
+const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+    
+  }
+  return results;
+
+  };
+
+// const results1 = map(words, (word) => word[0]);
+// console.log(results1);
+
+const results1 = map(words, (word) => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
