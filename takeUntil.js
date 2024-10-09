ddd95eed93424c0ba3127378d9bc05b9da1826f3
Gladdy17 <gladston.bosco@gmail.com>
2024-10-09 05:36:02 -0600

    added changes as requested by AI

--- takeUntil.js
+++ takeUntil.js
@@ -2,7 +2,7 @@ const takeUntil = function (array, callback) {
   const result = [];
 
   for (const element of array) {
-    if (callback === element) {
+    if (callback(element)) {
       break;
     }
     result.push(element);
