3eb12c56c3a389e0c40ada43b345b7c15195081c
Gladdy17 <gladston.bosco@gmail.com>
2024-10-09 05:55:01 -0600

    findkey_added

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (obj, callback) {
+  for (const element of Object.keys(obj)) {
+    if (callback(obj[element])) {
+      return element;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  )
+); // => "noma"
