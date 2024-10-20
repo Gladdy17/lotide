53ae610d632574c9d284a46e4aa49ca9a89ae3b1
Gladdy17 <gladston.bosco@gmail.com>
2024-10-20 11:57:51 -0600

    Refactored Test for assertArraysEquals

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,16 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function (arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -18,4 +20,4 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2]);
+module.exports = assertArraysEqual;
