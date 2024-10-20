eb8976b345a6e6db08291988ed1f9e517454d3ba
Gladdy17 <gladston.bosco@gmail.com>
2024-10-20 14:24:43 -0600

    Added Mocha Test

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,19 @@
 
-const assertEqual = require("../assertEqual");
+const assert = require("assert");
 const head = require("../head")
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual (head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+describe("#head", () => {
+    it("returns 5 for [5, 6, 7]", () => {
+      assert.strictEqual(head([5, 6, 7]), 5);
+    });
+
+    it("returns 5 for [5]", () => {
+      assert.strictEqual(head([5]), 5);
+    });
+    it("returns Hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+      assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+    });
+
+
+});
+
