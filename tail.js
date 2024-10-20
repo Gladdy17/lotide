c40d50c93bb320fc65e1fc9017394beb75a16a6f
Gladdy17 <gladston.bosco@gmail.com>
2024-10-20 11:33:45 -0600

    refactored eqArrays

--- tail.js
+++ tail.js
@@ -7,8 +7,5 @@ const tail = function(array) {
 module.exports = tail;
 
 
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
 
 
